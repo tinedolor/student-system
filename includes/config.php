d0907cfaa05594b3a7bc66ec43f3ca12c2052d38
tinedolor <justineromero.dolor@gmail.com>
2025-06-13 21:01:22 +0800

    last update

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'studentdatabase');
+define('DB_NAME', 'studentdb');
 
 // Site configuration
 define('SITE_NAME', 'Student Portal');
